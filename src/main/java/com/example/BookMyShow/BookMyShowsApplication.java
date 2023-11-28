4ee7b8b4e90eae9911c78aed5824e900a89a71ab
ramanaguru <ramanaguruloyola@gmail.com>
2023-11-28 19:45:58 +0530

    first commit

---
+++ src/main/java/com/example/BookMyShow/BookMyShowsApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmyshows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMyShowsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookMyShowsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/BookMyShow/BookMyShowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookmyshows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMyShowsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
